d60e92bf5e281845d46114c7aeae5d5f719604f2
heidi <heidi666@users.noreply.github.com>
2014-06-10 15:26:59 +0100

    Create imgur.sh

---
+++ imgur.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+scrot -s 'temp.png'
+test=`base64 temp.png`
+response=`curl -X POST 'https://api.imgur.com/3/image.json' -F 'image=@temp.png' --header "Authorization: Client-ID aa81f8c4b7443b8"`
+picid=${response:15:7}
+url='https://i.imgur.com/'$picid'.png'
+rm -f temp.png
+echo $url | xclip -selection clipboard
+notify-send 'Success: '$url
